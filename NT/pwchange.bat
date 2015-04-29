b0e622bebf77e10f7900ab016aae4fb62aa4e52c
Skip Cruse <f8al.err0r@gmail.com>
2015-04-29 16:48:30 -0500

    initial commit
    adding initial data

---
+++ NT/pwchange.bat
@@ -0,0 +1,27 @@
+@echo off
+
+<path-to-splunk-uf-installer>
+
+
+
+Setlocal EnableDelayedExpansion
+Set _RNDLength=16
+Set _Alphanumeric=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+Set _Str=%_Alphanumeric%987654321
+:_LenLoop
+IF NOT "%_Str:~18%"=="" SET _Str=%_Str:~9%& SET /A _Len+=9& GOTO :_LenLoop
+SET _tmp=%_Str:~9,1%
+SET /A _Len=_Len+_tmp
+Set _count=0
+SET _RndAlphaNum=
+:_loop
+Set /a _count+=1
+SET _RND=%Random%
+Set /A _RND=_RND%%%_Len%
+SET _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
+If !_count! lss %_RNDLength% goto _loop
+
+"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" edit user admin -password !_RndAlphaNum! -auth admin:changeme
+echo "pwchange ran!"
+@echo on
+del /F /Q /S "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA_PWChange"
